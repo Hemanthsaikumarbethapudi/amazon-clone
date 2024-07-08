d7ffdaeb31f4f30a35d210c290183522a6961b90
hemanth <bhemanthsaikumar04@gmail.com>
2024-07-08 22:57:14 +0530

    code modified

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Carousel = () => {
+    return (
+        <div className="bg-purple-500">Carousel</div>
+    )
+}
+export default Carousel;
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+import { Carousel } from "./Carousel"
+
+export const Homepage = () => {
+    return (
+        <div className="bg-amazonclone-background">
+            <div className="min-w-[1000px] max-w-[1500px] m-auto bg-blue-300">Testing</div>
+            <Carousel />
+        </div>
+    )
+}
+
+export default Homepage;
