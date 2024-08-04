207586c13c5953a1c05084b21f11525c7924d36a
hemanth <bhemanthsaikumar04@gmail.com>
2024-08-04 22:18:54 +0530

    code added

--- src/components/Carouselproducts.jsx
+++ src/components/Carouselproducts.jsx
@@ -2,16 +2,16 @@ import { Swiper, SwiperSlide } from "swiper/react"
 import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
-// import image0 from "../asserts/amazon_clone/images/product_0_small.jpg"
-// import image1 from "../asserts/amazon_clone/images/product_1_small.jpg"
-// import image2 from "../asserts/amazon_clone/images/product_2_small.jpg"
-// import image3 from "../asserts/amazon_clone/images/product_3_small.jpg"
-// import image4 from "../asserts/amazon_clone/images/product_4_small.jpg"
-// import image5 from "../asserts/amazon_clone/images/product_5_small.jpg"
-// import image6 from "../asserts/amazon_clone/images/product_6_small.jpg"
-// import image7 from "../asserts/amazon_clone/images/product_7_small.jpg"
-// import image8 from "../asserts/amazon_clone/images/product_8_small.jpg"
-// import image9 from "../asserts/amazon_clone/images/product_9_small.jpg"
+import image0 from "../asserts/amazon_clone/images/product_0_small.jpg"
+import image1 from "../asserts/amazon_clone/images/product_1_small.jpg"
+import image2 from "../asserts/amazon_clone/images/product_2_small.jpg"
+import image3 from "../asserts/amazon_clone/images/product_3_small.jpg"
+import image4 from "../asserts/amazon_clone/images/product_4_small.jpg"
+import image5 from "../asserts/amazon_clone/images/product_5_small.jpg"
+import image6 from "../asserts/amazon_clone/images/product_6_small.jpg"
+import image7 from "../asserts/amazon_clone/images/product_7_small.jpg"
+import image8 from "../asserts/amazon_clone/images/product_8_small.jpg"
+import image9 from "../asserts/amazon_clone/images/product_9_small.jpg"
 
 
 
@@ -28,7 +28,7 @@ const Carouselproducts = () => {
                 {
                     Array.from({ length: 9 }, (_, i) =>
                         <SwiperSlide key={i}>
-                            <img src={`../asserts/amazon_clone/images/product_${i}_small.jpg`} />
+                            <img src={`image${i}`} />
                         </SwiperSlide>
                     )
                 }
